96644a60f08d051bc42944e62a4a26201ded26d0
Trung Lam <trunglam107@gmail.com>
2022-12-10 12:29:09 -0800

    added comments

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
 let rating = 0;
 
 function ratingHandler(userRating){
+    //unselect all buttons
     for(let i = 1; i <= 5; i++){
         document.getElementById("rate-" + i).className = "rate-btn";
     }
+
+    //select user chosen button
     const selectedBtn = document.getElementById("rate-" + userRating);
     selectedBtn.className = "selected";
     rating = userRating;
 }
 
 function submitHandler() {
+    //if user chooses no rating, return
     if(rating === 0){
         alert("Please choose a rating.");
         return;
     }
     
+    //display thank you card
     const ratingContainer = document.getElementsByClassName("rating-container");
     const thankYouContainer = document.getElementsByClassName("thank-you-container");
     const rateSelection = document.getElementById("rating");
