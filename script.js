d6cc0f5e87d504d95c472be4dd4f8fbd88f0ca71
Trung Lam <trunglam107@gmail.com>
2022-12-10 12:22:41 -0800

    fix button focus

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let rating = 0;
 
 function ratingHandler(userRating){
+    for(let i = 1; i <= 5; i++){
+        document.getElementById("rate-" + i).className = "rate-btn";
+    }
+    const selectedBtn = document.getElementById("rate-" + userRating);
+    selectedBtn.className = "selected";
     rating = userRating;
 }
 
